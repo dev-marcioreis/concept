95d0df8429bc4982dfe28e297d755ea36658a125
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-22 15:32:17 -0300

    Section project

--- js/main.js
+++ js/main.js
@@ -135,3 +135,35 @@ function aDelay(t) {
       return 1;
     }
 }
+
+
+// Cards
+const cards = document.querySelectorAll('.card')
+
+cards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+
+    const state = Flip.getState(cards)
+    const isCardActive = card.classList.contains('active')
+
+    cards.forEach((otherCard, otherIndex) => {
+
+      otherCard.classList.remove('active')
+      otherCard.classList.remove('inactive')
+
+      if(!isCardActive && index !== otherIndex) {
+        otherCard.classList.add('inactive')
+      }
+
+    })
+
+    if(!isCardActive) card.classList.add('active')
+
+    Flip.from(state, {
+      duration: 1,
+      ease: "expo.out",
+      absolute: true
+    })
+
+  })
+})
